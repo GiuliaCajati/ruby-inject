2b5ae4bd47585484dd34082ce89dcbea08fbf795
giuliacajati <giuliacajati@gmail.com>
2021-06-07 12:27:29 -0500

    inject clean-up

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,27 +1,2 @@
-def compare value, expected
-  return if value == expected
-  puts "#{value.inspect} received, expecting #{expected.inspect}"
-end
 
-def override_inject(enumerable)
-  def enumerable.inject(n = nil)
-    is_empty = true
-
-    result = []
-    each do |item|
-      return item if n.nil? #if empty return nil 
-      is_empty = false
-
-      result << item
-      return result if result.length == n
-    end
-    return result unless is_empty
-
-    # If n is nil result is a value, else an array
-    n.nil? ? nil : []
-  end
-end
-
-a1 = [1, 2, 3]
-
-compare(inject(a1, :+), 6)
+# run.rb

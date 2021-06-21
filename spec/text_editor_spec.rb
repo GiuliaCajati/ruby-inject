e4f4510b93f15b5294222e378469bb578656bc35
giuliacajati <giuliacajati@gmail.com>
2021-06-21 08:48:12 -0500

    testing set-up

--- spec/text_editor_spec.rb
+++ spec/text_editor_spec.rb
@@ -1,1 +1,12 @@
+#testing my inject 
+require './lib/inject.rb'
 
+RSpec.describe TextEditor do
+  
+  describe 'inject' do
+    # before :each do
+    #   @editor = TextEditor.new
+    # end
+  end 
+
+end 
